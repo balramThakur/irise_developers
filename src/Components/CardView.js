d38b56db89e7709380845e6b7412bda091636b8d
balramThakur <99987850+balramThakur@users.noreply.github.com>
2024-04-02 23:14:10 +0530

    .

---
+++ src/Components/CardView.js
@@ -0,0 +1,17 @@
+// Card.js
+import React from 'react';
+import '../CSS/CardView.css'; // Import the CSS file for styling
+
+const CardView = ({ title, description, image }) => {
+  return (
+    <div className="card">
+      <img src={image} alt={title} className="card-image" />
+      <div className="card-content">
+        <h2 className="card-title">{title}</h2>
+        <p className="card-description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardView;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,18 +2,76 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import Carousel from '../Components/Carousel';
+import CardView from '../Components/CardView';
 
 function Home() {
   const items = [
-    <img src="https://source.unsplash.com/random/1920x1080/?buildings,landscape" height={600} alt="Image1" />,
+    <img src="https://source.unsplash.com/random/1920x1080/?Buildings,landscape" height={600} alt="Image1" />,
     <img src="https://source.unsplash.com/random/1920x1080/?lands,landscape" height={600} alt="Image2" />,
     <img src="https://source.unsplash.com/random/1920x1080/?wallpaper,landscape" height={600} alt="Image3" />,
     // Add more items as needed
   ];
+  const ongingProjects = [
+    {
+      title: 'Card 1',
+      description: 'Description for card 1',
+      image: 'https://source.unsplash.com/random/1920x1080/?building,landscape',
+    },
+    {
+      title: 'Card 2',
+      description: 'Description for card 2',
+      image: 'https://source.unsplash.com/random/1920x1080/?building,landscape',
+    },
+    {
+      title: 'Card 3',
+      description: 'Description for card 2',
+      image: 'https://source.unsplash.com/random/1920x1080/?building,landscape',
+    },
+    // Add more cards as needed
+  ];
+  const completedProjects = [
+    {
+      title: 'Card 1',
+      description: 'Description for card 1',
+      image: 'https://source.unsplash.com/random/1920x1080/?building,landscape',
+    },
+    {
+      title: 'Card 2',
+      description: 'Description for card 2',
+      image: 'https://source.unsplash.com/random/1920x1080/?building,landscape',
+    },
+    {
+      title: 'Card 3',
+      description: 'Description for card 2',
+      image: 'https://source.unsplash.com/random/1920x1080/?building,landscape',
+    },
+    // Add more cards as needed
+  ];
   return (
     <div>
       <Navbar />
-      <Carousel items={items} />
+      <div className="top_container">
+        <Carousel items={items} />
+        <div className="title_ongoing_projects">
+          <h1> ONGOING PROJECTS</h1>
+          <span className='underline'></span>
+        </div>
+        <div className='card_view'>
+          {ongingProjects.map((card, index) => (
+            <CardView key={index} title={card.title} description={card.description} image={card.image} />
+          ))}
+        </div>
+
+        <div className="title_ongoing_projects">
+          <h1> COMPLETED PROJECTS</h1>
+          <span className='underline'></span>
+        </div>
+        <div className='card_view'>
+          {completedProjects.map((card, index) => (
+            <CardView key={index} title={card.title} description={card.description} image={card.image} />
+          ))}
+        </div>
+      </div>
       <Footer />
     </div>
   )
